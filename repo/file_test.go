40770fc2bc18e2cafa4c3409bc490fe421bc42d8
EyesHuang <charliehuang1017@gmail.com>
2024-07-19 16:24:22 +0800

    Refactor unit tests for file domain on repo package

--- repo/file_test.go
+++ repo/file_test.go
@@ -2,8 +2,7 @@ package repo
 
 import (
 	"testing"
-
-	"vfs/mock"
+	"time"
 
 	"vfs"
 
@@ -13,52 +12,61 @@ import (
 func TestGetFile(t *testing.T) {
 	repo := NewMemoryFileRepo()
 
-	_ = repo.AddFile(mock.File1)
+	file := &vfs.File{
+		UserName:   "user1",
+		FolderName: "folder1",
+		Name:       "file1",
+	}
+	_ = repo.AddFile(file)
 
-	retrievedFile := repo.GetFile(mock.FileKeySet)
+	retrievedFile := repo.GetFile(vfs.FileKeySet{UserName: "user1", FolderName: "folder1", FileName: "file1"})
 	assert.NotNil(t, retrievedFile)
 	assert.Equal(t, "file1", retrievedFile.Name)
+
+	retrievedFile = repo.GetFile(vfs.FileKeySet{UserName: "user1", FolderName: "folder1", FileName: "nonexistent"})
+	assert.Nil(t, retrievedFile)
 }
 
-func TestGetFiles_SortyByName(t *testing.T) {
+func TestGetFiles(t *testing.T) {
 	repo := NewMemoryFileRepo()
 
-	_ = repo.AddFile(mock.File1)
-	_ = repo.AddFile(mock.File2)
-	_ = repo.AddFile(mock.File3)
+	now := time.Now()
+	file1 := &vfs.File{
+		UserName:   "user1",
+		FolderName: "folder1",
+		Name:       "file1",
+		CreatedAt:  now.Add(-2 * time.Hour),
+	}
+	file2 := &vfs.File{
+		UserName:   "user1",
+		FolderName: "folder1",
+		Name:       "file2",
+		CreatedAt:  now.Add(-1 * time.Hour),
+	}
+	file3 := &vfs.File{
+		UserName:   "user2",
+		FolderName: "folder2",
+		Name:       "file3",
+		CreatedAt:  now,
+	}
+	_ = repo.AddFile(file1)
+	_ = repo.AddFile(file2)
+	_ = repo.AddFile(file3)
 
-	files := repo.GetFiles(mock.GetFilesRequest)
+	req := &vfs.GetFilesRequest{
+		UserName:   "user1",
+		FolderName: "folder1",
+		SortBy:     vfs.Name,
+		OrderBy:    vfs.Asc,
+	}
+
+	files := repo.GetFiles(req)
 	assert.Len(t, files, 2)
 	assert.Equal(t, "file1", files[0].Name)
 	assert.Equal(t, "file2", files[1].Name)
-}
 
-func TestGetFiles_SortyByCreatedTime(t *testing.T) {
-	repo := NewMemoryFileRepo()
-
-	_ = repo.AddFile(mock.File1)
-	_ = repo.AddFile(mock.File2)
-	_ = repo.AddFile(mock.File3)
-
-	mock.GetFilesRequest.SortBy = vfs.Created
-
-	files := repo.GetFiles(mock.GetFilesRequest)
-	assert.Len(t, files, 2)
-	assert.Equal(t, "file1", files[0].Name)
-	assert.Equal(t, "file2", files[1].Name)
-}
-
-func TestGetFiles_SortyByCreatedTimeWithDescOrder(t *testing.T) {
-	repo := NewMemoryFileRepo()
-
-	_ = repo.AddFile(mock.File1)
-	_ = repo.AddFile(mock.File2)
-	_ = repo.AddFile(mock.File3)
-
-	mock.GetFilesRequest.SortBy = vfs.Created
-	mock.GetFilesRequest.OrderBy = vfs.Desc
-
-	files := repo.GetFiles(mock.GetFilesRequest)
+	req.OrderBy = vfs.Desc
+	files = repo.GetFiles(req)
 	assert.Len(t, files, 2)
 	assert.Equal(t, "file2", files[0].Name)
 	assert.Equal(t, "file1", files[1].Name)
@@ -67,17 +75,28 @@ func TestGetFiles_SortyByCreatedTimeWithDescOrder(t *testing.T) {
 func TestAddFile_Success(t *testing.T) {
 	repo := NewMemoryFileRepo()
 
-	err := repo.AddFile(mock.File1)
+	file := &vfs.File{
+		UserName:   "user1",
+		FolderName: "folder1",
+		Name:       "file1",
+	}
+
+	err := repo.AddFile(file)
 	assert.Nil(t, err)
-	assert.NotNil(t, repo.GetFile(mock.FileKeySet))
+	assert.NotNil(t, repo.GetFile(vfs.FileKeySet{UserName: "user1", FolderName: "folder1", FileName: "file1"}))
 }
 
 func TestAddFile_ExistingFile(t *testing.T) {
 	repo := NewMemoryFileRepo()
 
-	_ = repo.AddFile(mock.File1)
+	file := &vfs.File{
+		UserName:   "user1",
+		FolderName: "folder1",
+		Name:       "file1",
+	}
+	_ = repo.AddFile(file)
 
-	err := repo.AddFile(mock.File1)
+	err := repo.AddFile(file)
 	assert.NotNil(t, err)
 	assert.Equal(t, "The file1 has already existed.", err.Error())
 }
@@ -85,19 +104,22 @@ func TestAddFile_ExistingFile(t *testing.T) {
 func TestDeleteFile_Success(t *testing.T) {
 	repo := NewMemoryFileRepo()
 
-	_ = repo.AddFile(mock.File1)
+	file := &vfs.File{
+		UserName:   "user1",
+		FolderName: "folder1",
+		Name:       "file1",
+	}
+	_ = repo.AddFile(file)
 
-	err := repo.DeleteFile(mock.FileKeySet)
+	err := repo.DeleteFile(vfs.FileKeySet{UserName: "user1", FolderName: "folder1", FileName: "file1"})
 	assert.Nil(t, err)
-	assert.Nil(t, repo.GetFile(mock.FileKeySet))
+	assert.Nil(t, repo.GetFile(vfs.FileKeySet{UserName: "user1", FolderName: "folder1", FileName: "file1"}))
 }
 
 func TestDeleteFile_NotFound(t *testing.T) {
 	repo := NewMemoryFileRepo()
 
-	mock.FileKeySet.FileName = "nonexistent"
-
-	err := repo.DeleteFile(mock.FileKeySet)
+	err := repo.DeleteFile(vfs.FileKeySet{UserName: "user1", FolderName: "folder1", FileName: "nonexistent"})
 	assert.NotNil(t, err)
 	assert.Equal(t, "The nonexistent doesn't exist.", err.Error())
 }
